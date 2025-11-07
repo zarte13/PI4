849d39629235e20ec9a919a45ba7fe7f272ff708
emayer <emile.mayer@polymtl.ca>
2025-11-06 23:45:16 -0500

    ajout des fichiers pour scripts matlab de simulation de tests laser + IR

---
+++ matlab_simu/main.m
@@ -0,0 +1,24 @@
+%% Projet : MEC8370
+% Auteur : Émile Mayer
+% Dernière modification  : 4 novembre 2025
+
+%% Constantes
+
+% Constantes physiques
+T_fonte_glace = 273.15; % Kelvins
+Lambda_laser = 10e-6; % m
+Lambda_IR = ; % m CONFIRMER LA VALEUR SELON LAMPE 
+Coeff_abspt_glace_laser = ; % Constante CONFIRMER SELON LONGUEUR ONDE laser
+Coeff_abspt_glace_IR = ; % Constante CONFIRMER SELON LONGUEUR ONDE IR
+
+% Constantes paramètres
+P_laser = 100; % W
+P_IR = 100; % W
+Angle_ouverture_IR = 120; % Degrés
+ 
+
+% Constantes simulation
+D_mur_glace = 1; % m
+Cote_mur_glace = 1; % m
+Aire_mur_glace = Cote_mur_glace ^2; % m^2
+
